c22334043b352fb3486e77fca20daa12c25ff87b
lhommenul <idlhommenul@gmail.com>
2021-07-07 14:29:30 +0200

    update req file with arrow functions

--- js/req.mjs
+++ js/req.mjs
@@ -1,51 +1,50 @@
 export { 
     getTouits,
-    getTranding,
+    getTrending,
     getOneTouit,
     getMostActiveUser,
     sendComment,
     getTouitComment
 }
 // ====== GET TOUIT ======
-function getTouits() {
-    let xhttp = new XMLHttpRequest()
-    xhttp.onreadystatechange = ()=>{
-        if (xhttp.readyState === 4 && xhttp.status === 200) {
-            const cont = document.getElementsByClassName('cards_list_container')[0];
-            let i = JSON.parse(xhttp.responseText).messages.reverse()
-            for (let index = 0; index < 50; index++) {
-                const element = array[index];
-                cont.appendChild(new Card(element).generateHtml())   
+let getTouits = new Promise((resolve,reject) => {
+    try {
+        let xhttp = new XMLHttpRequest()
+        xhttp.onreadystatechange = ()=>{
+            if (xhttp.readyState === 4 && xhttp.status === 200) {
+                resolve(xhttp.responseText);
             }
         }
+        xhttp.open("GET","http://touiteur.cefim-formation.org/list")
+        xhttp.send()
+    } catch (error) {
+        reject(error);
     }
-    xhttp.open("GET","http://touiteur.cefim-formation.org/list")
-    xhttp.send()
-}
+})
 
 // ====== SEND TOUIT ======
-
-// document.getElementById('form_add_touit').addEventListener('submit',e=>{
-//     e.preventDefault();
-//     const inp_nickname = document.getElementById('inp_nickname'),
-//         inp_text_area_send_message = document.getElementById('inp_text_area_send_message');
-//     if (inp_nickname.value != "" && inp_text_area_send_message.value != "") {
-//         let xhttp = new XMLHttpRequest(),
-//             formData = new FormData();
-//         xhttp.onreadystatechange = ()=>{
-//             if (xhttp.readyState === 4 && xhttp.status === 200) {
-//                 console.log(xhttp.responseText);
-//             }
-//         }
-//         formData.append("name",inp_nickname.value)
-//         formData.append("message",inp_text_area_send_message.value)
-//         xhttp.open("POST",`http://touiteur.cefim-formation.org/send`)
-//         xhttp.send(formData)
-//     }else alert("Les champs ne sont pas good :/");
-// })
+let sendTouit = (inp_nickname,inp_text_area_send_message)=>{
+    return new Promise((resolve,reject)=>{
+        try {
+            let xhttp = new XMLHttpRequest(),
+            formData = new FormData();
+            xhttp.onreadystatechange = ()=>{
+                if (xhttp.readyState === 4 && xhttp.status === 200) {
+                    resolve(xhttp.responseText)
+                }
+            }
+            formData.append("name",inp_nickname)
+            formData.append("message",inp_text_area_send_message)
+            xhttp.open("POST",`http://touiteur.cefim-formation.org/send`)
+            xhttp.send(formData)      
+        } catch (error) {
+            reject(error)
+        }
+    })
+}
 
 // ====== GET TRANDING ======
-let getTranding = new Promise((resolve,reject)=>{
+let getTrending = new Promise((resolve,reject)=>{
     try {
         let xhr = new XMLHttpRequest()
         xhr.onreadystatechange = ()=>{
@@ -61,62 +60,83 @@ let getTranding = new Promise((resolve,reject)=>{
 })
 
 // ====== GET ONE TOUIT ======
-
-function getOneTouit(id) {
-    let xhr = new XMLHttpRequest()
-    xhr.onreadystatechange = ()=>{
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            console.log(xhr.responseText);
+let getOneTouit = (id)=>{return new Promise((resolve,reject) => {
+    try {
+        let xhr = new XMLHttpRequest()
+        xhr.onreadystatechange = ()=>{
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                resolve(xhr.responseText);
+            }
         }
+        xhr.open("GET",`http://touiteur.cefim-formation.org/get?id=${id}`)
+        xhr.send()
+    } catch (error) {
+        reject(error);
     }
-    xhr.open("GET",`http://touiteur.cefim-formation.org/get?id=${id}`)
-    xhr.send()
-}
+})}
 
 // ====== GET MOST ACTIVE USER ======
 
-function getMostActiveUser(count=1) {
-    let xhr = new XMLHttpRequest()
-    xhr.onreadystatechange = ()=>{
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            console.log(xhr.responseText);
+let getMostActiveUser = (count=1)=> {
+    return new Promise((resolve,reject)=>{
+        try {
+            let xhr = new XMLHttpRequest()
+            xhr.onreadystatechange = ()=>{
+                if (xhr.readyState === 4 && xhr.status === 200) {
+                    resolve(xhr.responseText)
+                }
+            }
+            xhr.open("GET",`http://touiteur.cefim-formation.org/influencers?count =${count}`)
+            xhr.send()
+        } catch (error) {
+            reject(error)        
         }
-    }
-    xhr.open("GET",`http://touiteur.cefim-formation.org/influencers?count =${count}`)
-    xhr.send()
+    })
 }
 
 // ====== GET MOST TOUIT LIKES ======
-function getMostTouitLike(count=1) {
-    let xhr = new XMLHttpRequest()
-    xhr.onreadystatechange = ()=>{
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            console.log(xhr.responseText);
+let getMostTouitLike = (count=1)=> {
+    return new Promise((resolve,reject)=>{
+        try {
+            let xhr = new XMLHttpRequest()
+            xhr.onreadystatechange = ()=>{
+                if (xhr.readyState === 4 && xhr.status === 200) {
+                    resolve(xhr.responseText)
+                }
+            }
+            xhr.open("GET",`http://touiteur.cefim-formation.org/likes/top?count =${count}`)
+            xhr.send()      
+        } catch (error) {
+            reject(error)
         }
-    }
-    xhr.open("GET",`http://touiteur.cefim-formation.org/likes/top?count =${count}`)
-    xhr.send()
+    })
 }
 
 // ====== SEND COMMENT ======
 // sendComment(8,"default name","default comment")
-function sendComment(message_id,name,comment) {
-    let formData = new FormData();
-        formData.append("message_id",message_id)
-        formData.append("name",name)
-        formData.append("comment",comment)
-    let xhr = new XMLHttpRequest()
-    xhr.onreadystatechange = ()=>{
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            console.log(xhr.responseText);
+let sendComment = (message_id,name,comment)=> {
+    return new Promise((resolve,reject)=>{
+            try {
+                let formData = new FormData();
+                formData.append("message_id",message_id)
+                formData.append("name",name)
+                formData.append("comment",comment)
+            let xhr = new XMLHttpRequest()
+            xhr.onreadystatechange = ()=>{
+                if (xhr.readyState === 4 && xhr.status === 200) {
+                    resolve(xhr.responseText)
+                }
+            }
+            xhr.open("POST",`http://touiteur.cefim-formation.org/comments/send`)
+            xhr.send(formData)
+        } catch (error) {
+            reject(error)
         }
-    }
-    xhr.open("POST",`http://touiteur.cefim-formation.org/comments/send`)
-    xhr.send(formData)
+    })
 }
 
 // ====== GET TOUIT COMMENT ======
-function getTouitComment(message_id) {
+let getTouitComment = (message_id)=>{
     return new Promise((resolve,reject)=>{
         try {
             let xhr = new XMLHttpRequest()
--- js/script.mjs
+++ js/script.mjs
@@ -1,12 +1,22 @@
-import {getTranding} from "./req.mjs"
+import { getTouits,
+    getTrending,
+    getOneTouit,
+    getMostActiveUser,
+    sendComment,
+    getTouitComment} from "./req.mjs"
 // globalThis
 
 
 
 
+
+
+
+// GET TREND
 (async ()=>{
     const container = document.getElementsByClassName('trends')[0];
-        getTranding.then(data=>{            
+        getTrending.then(data=>{    
+            console.log(data);        
             for (const pr in data) {
                 // CREATE BUBBLE
                 (()=>{
@@ -32,3 +42,31 @@ import {getTranding} from "./req.mjs"
         });
 })();
 
+(async ()=>{
+    const container = document.getElementsByClassName('touits')[0];
+        getTrending.then(data=>{    
+            console.log(data);        
+            for (const pr in data) {
+                // CREATE BUBBLE
+                (()=>{
+                    let li = document.createElement('li'),
+                        prop = document.createElement('p'),
+                        p = document.createElement('p');
+                    (()=>{
+                        li.className = "trend"
+                    })();
+                    // Set Data
+                    (()=>{
+                        prop.innerText = pr;
+                        p.innerText = data[pr];
+                    })();
+                    // Append
+                    (()=>{
+                        li.appendChild(prop)
+                        li.appendChild(p)
+                        container.appendChild(li)
+                    })();
+                })();
+            }
+        });
+})();
