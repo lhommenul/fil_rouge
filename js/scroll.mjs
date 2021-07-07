a9a240b0943b0c0ad32a9cd5c4f27f3656217166
lhommenul <idlhommenul@gmail.com>
2021-07-07 16:23:44 +0200

    update

--- js/scroll.mjs
+++ js/scroll.mjs
@@ -1,16 +1,20 @@
 export function Scroll(elements,index){
-    console.log(elements);
+    let result;
+    elements[index].element?.classList?.add("selected_touit")
     globalThis.addEventListener("mousewheel",(e)=>{
+        elements[index].element?.classList?.remove("selected_touit")
         let goUp = ()=>{
-            index = index<=0?elements.length:index--;
-            console.log(index);
+            index = index<elements.length-1?index+1:0;
             return index;
         }
         let goDown = ()=>{
-            index = index<=0?elements.length:index--;
-            console.log(index);
+            index = index<=0?elements.length-1:index-1;
             return index;
         }
-        return e.deltaY>=0?goUp():goDown();
+        result = e.deltaY>=0?goUp():goDown();
+        console.log(result);
+        elements[index].element?.classList?.add("selected_touit")
     });
+    return result;
 }
+
