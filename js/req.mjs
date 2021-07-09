f06f8e73fc8eb23b62a4e3824cf9c08088e956c9
lhommenul <idlhommenul@gmail.com>
2021-07-09 09:16:29 +0200

    update

--- js/req.mjs
+++ js/req.mjs
@@ -6,7 +6,8 @@ export {
     sendComment,
     sendTouit,
     getTouitComment,
-    createBubble
+    createBubble,
+    likeTouit
 }
 // ====== GET TOUIT ======
 let getTouits = new Promise((resolve,reject) => {
@@ -23,6 +24,26 @@ let getTouits = new Promise((resolve,reject) => {
         reject(error);
     }
 })
+
+// ====== LIKE TOUIT ======
+let likeTouit = (message_id)=>{
+    return new Promise((resolve,reject) => {
+        try {
+            let xhttp = new XMLHttpRequest();
+            let formData = new FormData();
+            formData.append("message_id",message_id)
+            xhttp.onreadystatechange = ()=>{
+                if (xhttp.readyState === 4 && xhttp.status === 200) {
+                    resolve(xhttp.responseText);
+                }
+            }
+            xhttp.open("PUT",`http://touiteur.cefim-formation.org/likes/send`)
+            xhttp.send(formData)
+        } catch (error) {
+            reject(error);
+        }
+    })
+}
 
 // ====== SEND TOUIT ======
 let sendTouit = (inp_nickname,inp_text_area_send_message)=>{
@@ -147,7 +168,7 @@ let getTouitComment = (message_id)=>{
                     resolve(xhr.responseText)
                 }
             }
-            xhr.open("GET",`http://touiteur.cefim-formation.org/comments/list?message_id=${message_id }`)
+            xhr.open("GET",`http://touiteur.cefim-formation.org/comments/list?message_id=${message_id}`)
             xhr.send()
         } catch (error) {
             reject(error)
@@ -162,6 +183,13 @@ let createBubble = (data,active_user)=>{
     let li = document.createElement('li'),
         likes = document.createElement('span'),
         name = document.createElement('p'),
+        button_like = document.createElement('button'),
+        button_comment = document.createElement('button'),
+        form = document.createElement('form'),
+        form_name = document.createElement('input'),
+        form_textarea = document.createElement('textarea'),
+        form_btn = document.createElement('button'),
+        comment = document.createElement('ul'),
         message = document.createElement('p');
     (()=>{
         li.className = "touit"
@@ -179,24 +207,62 @@ let createBubble = (data,active_user)=>{
         // message: "defined"
         // name: "defined"
         // ts: 1622627971
-        likes.innerText = data.likes; 
-        name.innerText = data.name;
-        message.innerText = data.message;
+        button_like.textContent = "like"
+        button_comment.textContent = "comment"
+        likes.textContent = data.likes; 
+        name.textContent = data.name;
+        form_btn.textContent = "envoyer";
+        message.textContent = data.message;
     })();
     // Append
     (()=>{
         li.appendChild(likes)
         li.appendChild(name)
         li.appendChild(message)
+        li.appendChild(button_like)
+        li.appendChild(button_comment)
+        li.appendChild(form)
+        form.appendChild(form_name)
+        form.appendChild(form_textarea)
+        form.appendChild(form_btn)
+        li.appendChild(comme nt)
     })();       
     // event
     (()=>{
-        li.addEventListener('click',()=>{
+        // open comment space
+        button_comment.addEventListener('click',()=>{
             getTouitComment(data.id)
             .then(data=>{
-                console.log(JSON.parse(data));
+                JSON.parse(data).comments.forEach(v=>{
+                    const li_c = document.createElement('li'),
+                        name_c = document.createElement('p'),
+                        p_c = document.createElement('p');
+                    name_c.innerText = v.name;
+                    p_c.innerText = v.comment;
+                    // Append
+                    (()=>{
+                        li_c.appendChild(name_c)
+                        li_c.appendChild(p_c)
+                        comment.appendChild(li_c)
+                    })();
+                })
+            })
+        })
+        // like touit
+        button_like.addEventListener('click',()=>{
+            likeTouit(data.id).then(e=>{
+                if (JSON.parse(e).success) {
+                    likes.innerText = parseInt(likes.innerText)+1;
+                }
+            })
+        })
+        //envoyer comment
+        form.addEventListener('submit',e=>{
+            e.preventDefault();
+            sendComment(data.id,form_name.value,form_textarea.value).then(res=>{
+                console.log(res);
             })
         })
     })();
-    return li
+    return li;
 }
